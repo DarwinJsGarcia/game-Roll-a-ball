f414ab2b88af8bd0a55201ec600c24361b1f07a5
unknown <darwingarcia930@gmail.com>
2020-10-21 01:18:35 -1100

    proyecto de juego

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private int contador;
+
+    public float velocity;
+
+    public Text TextoContador, TextoGanar;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        contador = 0;
+        setTextoContador();
+        TextoGanar.text="";
+    }
+
+    
+    void FixedUpdate ()
+    {
+        float movimientoH = Input.GetAxis ("Horizontal");
+        float movimientoV = Input.GetAxis ("Vertical");
+
+        Vector3 movimiento = new Vector3 (movimientoH, 0.0f,movimientoV);
+
+        rb.AddForce(movimiento * velocity);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("coleccionable"))
+        {
+            other.gameObject.SetActive(false);
+
+            contador = contador +1;
+
+            setTextoContador();
+        }
+    }
+        void setTextoContador(){
+        
+    
+        TextoContador.text = "Contador: " + contador.ToString();
+        if (contador >= 12){
+        TextoGanar.text = "¡Ganaste!";
+        }
+
+        }
+    }
+
